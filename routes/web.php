8022a66a085b31ec848d6a534ea8ef29c5b0e77e
Ikkew-san <ekkarat.no@gmail.com>
2018-04-10 16:19:16 +0700

    test

--- routes/web.php
+++ routes/web.php
@@ -22,4 +22,4 @@ $router->get('/key', function() {
  $router->get('/users','usersController@users');
  $router->get('/find-users/{id:[0-9]+}','usersController@find');
  $router->post('/create-users','usersController@create');
- //$router->put('/update-users','usersController@update');
+ $router->put('/update-users','usersController@update'); //test
